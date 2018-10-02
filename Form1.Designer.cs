8cf703b9c01ac9642f3f7235f5cb42e38c8b8add
Anuradha <anu.nampally@gmail.com>
2018-10-02 15:36:58 +0100

    Second Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DGVPlayers = new System.Windows.Forms.DataGridView();
+            this.PlayerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PlayerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Best_Round = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Worst_Round = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPlayers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVPlayers
+            // 
+            this.DGVPlayers.BackgroundColor = System.Drawing.Color.Turquoise;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVPlayers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGVPlayers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVPlayers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PlayerID,
+            this.PlayerName,
+            this.Best_Round,
+            this.Worst_Round});
+            this.DGVPlayers.Location = new System.Drawing.Point(-5, 2);
+            this.DGVPlayers.Name = "DGVPlayers";
+            this.DGVPlayers.Size = new System.Drawing.Size(573, 437);
+            this.DGVPlayers.TabIndex = 0;
+            // 
+            // PlayerID
+            // 
+            this.PlayerID.HeaderText = "Player_ID";
+            this.PlayerID.Name = "PlayerID";
+            // 
+            // PlayerName
+            // 
+            this.PlayerName.HeaderText = "Player_Name";
+            this.PlayerName.Name = "PlayerName";
+            // 
+            // Best_Round
+            // 
+            this.Best_Round.HeaderText = "Best_Round";
+            this.Best_Round.Name = "Best_Round";
+            // 
+            // Worst_Round
+            // 
+            this.Worst_Round.HeaderText = "Worst_Round";
+            this.Worst_Round.Name = "Worst_Round";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(570, 430);
+            this.Controls.Add(this.DGVPlayers);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Players List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPlayers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVPlayers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PlayerName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Best_Round;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Worst_Round;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, BestWorstRounds> rounds;
+        public Dictionary<string, Player> Players;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //on form load attaching data to data grid view
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Solution solution = new Solution();
+
+            //getting under par players and their best and worst rounds
+            rounds = solution.GetBestWorstRounds();
+
+            //getting all players.
+            Players = solution.GetPlayersAsDictionary();
+            int counter = 0;
+
+            //looping through the round dictionary and adding data to data grid view.
+            foreach (string key in rounds.Keys)
+            {
+                int index = DGVPlayers.Rows.Add();
+                if (counter < 3)
+                {
+                    DGVPlayers.Rows[index].DefaultCellStyle.BackColor = Color.Yellow;
+                    counter++;
+                }
+                DGVPlayers.Rows[index].Cells[0].Value = key;
+                DGVPlayers.Rows[index].Cells[1].Value = Players[key].player_bio.first_name + " " + Players[key].player_bio.last_name;
+                DGVPlayers.Rows[index].Cells[2].Value = rounds[key]._Best_Round;
+                DGVPlayers.Rows[index].Cells[3].Value = rounds[key]._Worst_Round;
+
+            }           
+        }
+    }
+
+    public class Solution
+    {
+        List<string> ListOfUnderParPlayers = new List<string>();
+        Dictionary<string, Player> ListOfPlayers = new Dictionary<string, Player>();
+        Dictionary<string, BestWorstRounds> ListofBestWorstRounds = new Dictionary<string, BestWorstRounds>();
+        List<string> ListOfTopGolfers = new List<string>();
+        Dictionary<string, string> ListOfPlayersCurrentPosition = new Dictionary<string,string>();
+
+        public Solution()
+        {
+            WebRequest request = WebRequest.Create("https://statdata.pgatour.com/r/009/leaderboard-v2mini.json");
+            request.Method = "GET";
+
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/json; charset=utf-8";
+
+            //Get response from web service
+            WebResponse response = request.GetResponse();
+
+            using (var s = response.GetResponseStream())
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    var ResponseInJson = sr.ReadToEnd();
+                    var DeserialisedResponse = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<RootObject>(ResponseInJson);
+
+                    foreach (Player p in DeserialisedResponse.leaderboard.players)
+                    {
+                        int flag = 0;
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (p.rounds[i].strokes < Convert.ToInt32(DeserialisedResponse.leaderboard.courses[0].par_total))
+                            {
+                                flag = 1;
+                            }
+                            else
+                            {
+                                flag = 0;
+                                break;
+                            }
+                        }
+                        if (flag == 1)
+                        {
+                            ListOfUnderParPlayers.Add(p.player_id);
+                        }
+
+
+                        ListOfPlayers.Add(p.player_id.ToString(), p);
+                    }                    
+                }
+            }
+        }        
+
+        //To get list of players.
+        public Dictionary<string, Player> GetPlayersAsDictionary()
+        {
+            return ListOfPlayers;
+        }
+
+        //To get list of players who are under par
+        public List<string> GetUnderParPlayers()
+        {
+            return ListOfUnderParPlayers;
+        }
+
+        //To get list of players who are under par and their best, worst rounds.
+        public Dictionary<string, BestWorstRounds> GetBestWorstRounds()
+        {            
+            foreach (string playerId in ListOfUnderParPlayers)
+            {
+                if (ListOfPlayers.ContainsKey(playerId))
+                {
+                    Player p = ListOfPlayers[playerId];
+                    int Min = int.MaxValue, Max = 0;
+                    int best_Round = 0, worst_Round = 0;
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (p.rounds[i].strokes < Min)
+                        {
+                            Min = Convert.ToInt32(p.rounds[i].strokes);
+                            best_Round = i;
+                            best_Round = ++best_Round;
+                        }
+
+                        if (p.rounds[i].strokes > Max)
+                        {
+                            Max = (int)p.rounds[i].strokes;
+                            worst_Round = i;
+                            worst_Round = ++worst_Round;
+                        }
+                        
+                    }  
+                  
+                    BestWorstRounds rounds = new BestWorstRounds();
+                    rounds._Best_Round = best_Round;
+                    rounds._Worst_Round = worst_Round;
+                    ListofBestWorstRounds.Add(playerId, rounds);
+                }
+            }
+            return ListofBestWorstRounds;
+        }        
+    }
+
+    public class BestWorstRounds
+    {
+        public int _Best_Round{ get; set;}
+        public int _Worst_Round { get; set; }
+    }
+}
